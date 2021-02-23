f6be6f388f56ba913cc220597b2b9a4693a9e080
Vincent de Almeida <vincent@decentralisedengineeringltd.co.uk>
2021-02-23 16:33:56 +0000

    deploy script for new staking contract

---
+++ scripts/10_deploy_staking_with_guild.js
@@ -0,0 +1,43 @@
+const prompt = require('prompt-sync')();
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  console.log(
+    "Deploying staking contract with guild using the account:",
+    await deployer.getAddress()
+  );
+
+  const tokenAddress = prompt('LPT token address? ');
+  const maxLPTRewards = prompt('Max $LPOOL rewards? ');
+  const startBlock = prompt('Start block? ');
+  const endBlock = prompt('End block? ');
+
+  console.log('LPT token', tokenAddress);
+  console.log('Max LPT rewards', maxLPTRewards);
+  console.log('Start block', startBlock);
+  console.log('End block', endBlock);
+
+  prompt('If happy, hit enter...');
+
+  const LaunchPoolStakingFactory = await ethers.getContractFactory("LaunchPoolStakingWithGuild");
+
+  const staking = await LaunchPoolStakingFactory.deploy(
+    tokenAddress,
+    ethers.utils.parseEther(maxLPTRewards),
+    startBlock,
+    endBlock
+  );
+
+  await staking.deployed();
+
+  console.log('Staking deployed at', staking.address);
+
+  console.log('Finished!');
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
