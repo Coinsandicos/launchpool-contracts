58118ecc7bbeeb22d0cc3ca277abfab7e3d08328
Vincent de Almeida <vincent@decentralisedengineeringltd.co.uk>
2021-02-24 12:11:22 +0000

    scripts

--- scripts/x_test_mass_update.js
+++ scripts/x_test_mass_update.js
@@ -1,12 +1,11 @@
 const prompt = require('prompt-sync')();
 
 const LaunchPoolStakingMetadata = require('../artifacts/LaunchPoolStakingWithGuild.json');
-const ERC20Metadata = require('../artifacts/StakingERC20.json');
 
 async function main() {
   const [deployer] = await ethers.getSigners();
   console.log(
-    "Setting up guild rewards using the account:",
+    "Mass update using the account:",
     await deployer.getAddress()
   );
 
@@ -20,17 +19,7 @@ async function main() {
     deployer //provider
   );
 
-  const guildAddress = await staking.rewardGuildBank()
-
-  console.log('guild addr', guildAddress)
-
-  const token = new ethers.Contract(
-    '0x6149C26Cd2f7b5CCdb32029aF817123F6E37Df5B',
-    ERC20Metadata.abi,
-    deployer //provider
-  );
-
-  console.log('balance of guild', (await token.balanceOf(guildAddress)).toString())
+  await staking.massUpdatePools()
 
   console.log('Finished!');
 }
