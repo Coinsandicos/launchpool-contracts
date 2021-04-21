38cb76afd5d707213534052aa18812d10b35d5fc
Vincent de Almeida <vincent@decentralisedengineeringltd.co.uk>
2021-04-21 15:37:28 +0100

    simpler script

--- scripts/24_phase_1_updates.js
+++ scripts/24_phase_1_updates.js
@@ -19,77 +19,52 @@ async function main() {
   );
 
   const LP8PoolId = 17
+  const UNIV2PoolId = 39
 
   const updates = [
-    [
-      [1, 0], // STPT
-      [LP8PoolId, 105881 + 14705],  // LP8
-    ],
-    [
-      [2, 0], // SHR
-      [LP8PoolId, 120586 + 14705] // LP8
-    ],
-    [
-      [4, 0], // POLS
-      [LP8PoolId, 135291 + 20588] // LP8
-    ],
-    [
-      [5, 0], // BONDLY
-      [LP8PoolId, 155879 + 14705] // LP8
-    ],
-    [
-      [6, 0], // RUNE Thor
-      [LP8PoolId, 170584 + 14705] // LP8
-    ],
-    [
-      [7, 0], // UNI
-      [LP8PoolId, 185289 + 20588] // LP8
-    ],
-    [
-      [8, 0], // SUSHI
-      [LP8PoolId, 205877 + 20588] // LP8
-    ],
-    [
-      [9, 0], // 1INCH
-      [LP8PoolId, 226465 + 20588] // LP8
-    ]
+    [2, LP8PoolId], // SHR -> LP8
+    [5, LP8PoolId], // BONDLY -> LP8
+    [6, LP8PoolId], // RUNE Thor -> LP8
+    [7, LP8PoolId], // UNI -> LP8
+    [8, LP8PoolId], // SUSHI -> LP8
+    [9, LP8PoolId], // 1INCH -> LP8
+
+    // Re-direct to UNI-v2
+    [1, UNIV2PoolId], // STPT -> UniV2
+    [4, UNIV2PoolId], // POLS -> UniV2
   ]
 
   console.log(`${updates.length} updates to perform`);
 
-  for(let i = 0; i < updates.length; i++) {
-    const pools = updates[i]
+  for (let j = 0; j < updates.length; j++) {
+    const [fromPid, toPid] = updates[j]
 
-    prompt(`\nUpdate ${ i + 1 } - hit enter to continue\n`)
+    const fromPoolInfo = await staking.poolInfo(fromPid)
+    const toPoolInfo = await staking.poolInfo(toPid)
 
-    for (let j = 0; j < pools.length; j++) {
-      const [pid, allocPoint] = pools[j]
+    const toPoolNewAlloc = toPoolInfo.allocPoint.add(fromPoolInfo.allocPoint)
 
-      const poolInfo = await staking.poolInfo(pid)
+    prompt(`Setting alloc of ${fromPid} to 0 and increasing ${toPid} to ${toPoolNewAlloc.toString()}`);
 
-      prompt(`Updating pool ${pid} to alloc point ${allocPoint} - hit enter to continue`);
+    await staking.set(
+      fromPid.toString(),
+      '0',
+      fromPoolInfo.maxStakingAmountPerUser,
+      true // mass update true
+    );
 
-      /// @notice Update a pool's allocation point to increase or decrease its share of contract-level rewards
-      /// @notice Can also update the max amount that can be staked per user
-      /// @dev Can only be called by the owner
-      /// @param _pid ID of the pool being updated
-      /// @param _allocPoint New allocation point
-      /// @param _maxStakingAmountPerUser Maximum amount that a user can deposit into the far
-      /// @param _withUpdate Set to true if you want to update all pools before making this change - it will checkpoint those rewards
-      // function set(uint256 _pid, uint256 _allocPoint, uint256 _maxStakingAmountPerUser, bool _withUpdate) public onlyOwner {
+    await staking.set(
+      toPid.toString(),
+      toPoolNewAlloc,
+      toPoolInfo.maxStakingAmountPerUser,
+      true // mass update true
+    );
 
-      await staking.set(
-        pid.toString(),
-        allocPoint.toString(),
-        poolInfo.maxStakingAmountPerUser,
-        true // mass update true
-      );
+    const fromPoolInfoAfter = await staking.poolInfo(fromPid)
+    const toPoolInfoAfter = await staking.poolInfo(toPid)
 
-      console.log(`pool ${pid} updated with weighting of ${allocPoint.toString()}`);
-
-      const lp8PoolInfo = await staking.poolInfo(LP8PoolId)
-      console.log(`LP8 new alloc point is: ${lp8PoolInfo.allocPoint.toString()}`)
-    }
+    console.log(`Pool ID ${fromPid} alloc after: ${fromPoolInfoAfter.allocPoint.toString()}`)
+    console.log(`Pool ID ${toPid} alloc after: ${toPoolInfoAfter.allocPoint.toString()}`)
   }
 
   console.log('Finished!');
